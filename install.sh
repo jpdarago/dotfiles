ae803825c10b1469c63db079182e9580b0def024
Juan Pablo Darago <jpdarago@gmail.com>
2020-10-04 13:34:26 +0200

    Use curl better

--- install.sh
+++ install.sh
@@ -3,19 +3,15 @@ set -euo
 set -x
 
 # Install dependencies
-sudo apt install curl
+sudo apt install git curl
 
 # Install NeoVim
-mkdir -p ~/bin
-curl -fL -o ~/bin/nvim https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
+curl -fLo ~/bin/nvim --create-dirs \
+    https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
 chmod u+x ~/bin/nvim
 
 # Install Z4H
-if command -v curl >/dev/null 2>&1; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v2/install)"
-else
-  sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v2/install)"
-fi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v2/install)"
 
 # Install VimPlug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
