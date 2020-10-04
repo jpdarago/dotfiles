873e288b11d065b9b5aa39e642acba6921dff6ad
Juan Pablo Darago <jpdarago@gmail.com>
2020-10-04 12:46:18 +0200

    Add neovim and curl to the install script

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 set -euo
 set -x
 
+# Install dependencies
+sudo apt install curl
+
+# Install NeoVim
+mkdir -p ~/bin
+curl -fL -o ~/bin/nvim https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
+chmod u+x ~/bin/nvim
+
 # Install Z4H
 if command -v curl >/dev/null 2>&1; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v2/install)"
